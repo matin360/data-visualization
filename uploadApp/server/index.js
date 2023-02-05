08e7b4bc48e0c18005b43e62a02a585da458307f
Матин Манафов <mamana@ttu.ee>
2023-02-05 14:33:22 +0200

    add file uploader app

---
+++ uploadApp/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const multer  = require('multer')
+const cors = require('cors');
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST']
+}));
+
+// setup multer for file upload
+var storage = multer.diskStorage(
+    {
+        destination: './uploads',
+        filename: function (req, file, cb ) {
+            cb( null, file.originalname);
+        }
+    }
+);
+
+const upload = multer({ storage: storage } )
+
+app.use(express.json());
+// serving front end build files
+app.use(express.static(__dirname + "/../uploads"));
+
+// route for file upload
+app.post("/api/uploadfile", upload.single('myFile'), (req, res, next) => {
+    console.log(req.file.originalname + " file successfully uploaded !!");
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
---
+++ uploadApp/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css'
+
+export default function App() {
+  const [file, setFile] = useState();
+
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("myFile", file);
+
+    console.log(file);
+    axios.post("http://localhost:3000/api/uploadfile", formData, {
+      headers: {
+        "content-type": "multipart/form-data",
+        "Access-Control-Allow-Origin": "*"
+      },
+    });
+  }
+
+  return (
+    <div className="App">
+      <div>
+      <input type="file" onChange={handleFileChange} />
+
+      <div>{file && `${file.name} - ${file.type}`}</div>
+
+      <button onClick={handleSubmit}>Upload</button>
+      </div>
+    </div>
+  )
+};
